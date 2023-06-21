9b341ee8af0691b59112d656fc28b575e4cc39d2
Ahmed <benzemaa46@gmail.com>
2023-06-22 02:11:48 +0300

    Initial commit

--- lib/presentation/resource/theme_manager.dart
+++ lib/presentation/resource/theme_manager.dart
@@ -40,7 +40,7 @@ ThemeData getApplicationTheme() {
       style: ElevatedButton.styleFrom(
           textStyle: getRegularStyle(
             color: ColorManager.primary,
-            fontSize: AppSize.s17,
+            fontSize: AppSize.,
           ),
           backgroundColor: ColorManager.primary,
           shape: RoundedRectangleBorder(

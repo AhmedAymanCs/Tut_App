3305b7ea601f5be40013e4d0a7fe4d77ab0de36f
Ahmed <benzemaa46@gmail.com>
2023-06-22 17:30:34 +0300

    Initial commit

---
+++ lib/presentation/resource/routes_manager.dart
@@ -0,0 +1,56 @@
+import 'package:acourse/presentation/resource/color_manager.dart';
+import 'package:acourse/presentation/resource/string_manager.dart';
+import 'package:acourse/presentation/resource/styles_manager.dart';
+import 'package:acourse/presentation/screens/forgot_password/forgot_password_view.dart';
+import 'package:acourse/presentation/screens/login/login_view.dart';
+import 'package:acourse/presentation/screens/register/register_view.dart';
+import 'package:acourse/presentation/screens/splash/splash_view.dart';
+import 'package:acourse/presentation/screens/store_details/store_details_view.dart';
+import 'package:flutter/material.dart';
+
+import '../screens/main/main_view.dart';
+
+class Routes
+{
+  static const String splashRoute = '/';
+  static const String loginRoute = '/login';
+  static const String registerRoute = '/register';
+  static const String forgotPasswordRoute = '/forgotPassword';
+  static const String mainRoute = '/main';
+  static const String storeDetailsRoute = '/storeDetails';
+}
+
+class RouteGenerator
+{
+  static Route<dynamic> getRoute(RouteSettings settings)
+  {
+    switch (settings.name)
+    {
+      case Routes.splashRoute : return MaterialPageRoute(builder: (_)=>const SplashView());
+      case Routes.loginRoute : return MaterialPageRoute(builder: (_)=> const LoginView());
+      case Routes.registerRoute : return MaterialPageRoute(builder: (_)=> const RegisterView());
+      case Routes.forgotPasswordRoute : return MaterialPageRoute(builder: (_)=> const ForgotPasswordView());
+      case Routes.mainRoute : return MaterialPageRoute(builder: (_)=>const MainView());
+      case Routes.storeDetailsRoute : return MaterialPageRoute(builder: (_)=>const StoreDetailsView());
+
+      default: return getUnDefinededRoute();
+    }
+  }
+
+  static Route<dynamic> getUnDefinededRoute()
+  {
+    return MaterialPageRoute(builder: (_)=>Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          StringsManager.unDefinededRoute
+        ),
+      ),
+      body: Center(
+        child: Text(
+          StringsManager.unDefinededRoute,
+          style: getBoldStyle(color: ColorManager.grey),
+        ),
+      ),
+    ));
+  }
+}
---
+++ lib/presentation/screens/main/main_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({Key? key}) : super(key: key);
+  @override
+  State<MainView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<MainView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
